b6b7b6bfb3a1d84ab68fca8f4a7d0e804316474c
Billy Todd <billy.todd50@yahoo.com>
2019-07-24 03:44:10 +0000

    middleware is safe comment out to deploy

--- middleware/index.js
+++ middleware/index.js
@@ -51,14 +51,14 @@ middlewareObj.isLoggedIn = function(req, res, next) {
 	res.redirect('/login');
 };
 
-middlewareObj.isSafe = function(req, res, next) {
-    if(req.body.image.match(/^https:\/\/images\.unsplash\.com\/.*/)) {
-      next();
-    }else {
-      req.flash('error', 'Only images from images.unsplash.com allowed.\nSee https://youtu.be/Bn3weNRQRDE for how to copy image urls from unsplash.');
-      res.redirect('back');
-    }
-};
+// middlewareObj.isSafe = function(req, res, next) {
+//     if(req.body.image.match(/^https:\/\/images\.unsplash\.com\/.*/)) {
+//       next();
+//     }else {
+//       req.flash('error', 'Only images from images.unsplash.com allowed.\nSee https://youtu.be/Bn3weNRQRDE for how to copy image urls from unsplash.');
+//       res.redirect('back');
+//     }
+// };
 
 
 module.exports = middlewareObj;
